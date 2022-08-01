eb6675019811e01f522d8f962cfc61b8bf5a04ac
Deni Gunawan <artdengun@gmail.com>
2022-08-01 13:43:41 +0700

    update

---
+++ src/main/java/com/deni/gunawan/backendspringvue/BackendSpringVueApplication.java
@@ -0,0 +1,13 @@
+package com.deni.gunawan.backendspringvue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendSpringVueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendSpringVueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/deni/gunawan/backendspringvue/BackendSpringVueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deni.gunawan.backendspringvue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendSpringVueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
